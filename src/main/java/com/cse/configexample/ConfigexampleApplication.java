3a6fef295b99a1097aefda7529cc4c15356182a1
Shilpa Repala <shilpa.repala@gmail.com>
2019-07-16 23:12:36 -0500

    first commit

---
+++ src/main/java/com/cse/configexample/ConfigexampleApplication.java
@@ -0,0 +1,15 @@
+package com.cse.configexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigexampleApplication.class, args);
+	}
+
+}
